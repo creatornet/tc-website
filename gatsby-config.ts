e24fe36206dcfaad3e89e35847c086d39a1d4cf2
Creator Network <120567699+creatornet@users.noreply.github.com>
2023-03-01 21:39:41 +0900

    Update gatsby-config.ts

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,8 +3,7 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `tc-website`,
-    description: `ゲーム開発者集会へようこそ！近日中にVRChatでイベントを開催予定です`,
-    author: `@creatornet`,
+    siteUrl:"https://creatornet.github.io/tc-website/"
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
